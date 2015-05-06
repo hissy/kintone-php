808bf8b2131d2be17ce4bb73618a88b9dfcf8cc5
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2015-05-06 22:15:07 +0900

    Do not redirect during api call

--- src/Request.php
+++ src/Request.php
@@ -9,11 +9,11 @@ class Request
     const API_BASE_URL = 'https://{subdomain}.cybozu.com/k/{version}/';
     const API_HOST = '{subdomain}.cybozu.com:443';
     const API_URL_EXT = '.json';
-    
+
     private $token = '';
     private $subdomain = '';
     private $client;
-    
+
     public function __construct($subdomain, $token)
     {
         $client = new GuzzleClient([
@@ -32,39 +32,60 @@ class Request
         $this->token = $token;
         $this->client = $client;
     }
-    
+
     public function get($url = null, array $options = [])
     {
-        return $this->client->get($url . self::API_URL_EXT, ['json' => $options]);
+        return $this->client->get($url . self::API_URL_EXT, [
+            'json' => $options,
+            'allow_redirects' => false
+        ]);
     }
-    
+
     public function head($url = null, array $options = [])
     {
-        return $this->client->head($url . self::API_URL_EXT, ['json' => $options]);
+        return $this->client->head($url . self::API_URL_EXT, [
+            'json' => $options,
+            'allow_redirects' => false
+        ]);
     }
-    
+
     public function delete($url = null, array $options = [])
     {
-        return $this->client->delete($url . self::API_URL_EXT, ['json' => $options]);
+        return $this->client->delete($url . self::API_URL_EXT, [
+            'json' => $options,
+            'allow_redirects' => false
+        ]);
     }
-    
+
     public function put($url = null, array $options = [])
     {
-        return $this->client->put($url . self::API_URL_EXT, ['json' => $options]);
+        return $this->client->put($url . self::API_URL_EXT, [
+            'json' => $options,
+            'allow_redirects' => false
+        ]);
     }
-    
+
     public function patch($url = null, array $options = [])
     {
-        return $this->client->patch($url . self::API_URL_EXT, ['json' => $options]);
+        return $this->client->patch($url . self::API_URL_EXT, [
+            'json' => $options,
+            'allow_redirects' => false
+        ]);
     }
-    
+
     public function post($url = null, array $options = [])
     {
-        return $this->client->post($url . self::API_URL_EXT, ['json' => $options]);
+        return $this->client->post($url . self::API_URL_EXT, [
+            'json' => $options,
+            'allow_redirects' => false
+        ]);
     }
-    
+
     public function options($url = null, array $options = [])
     {
-        return $this->client->options($url . self::API_URL_EXT, ['json' => $options]);
+        return $this->client->options($url . self::API_URL_EXT, [
+            'json' => $options,
+            'allow_redirects' => false
+        ]);
     }
 }
