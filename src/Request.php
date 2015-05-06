72ef6e0d3721adda17172823f56175b3fab41fc0
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2015-05-06 22:32:17 +0900

    Avoid 400 error with empty option

--- src/Request.php
+++ src/Request.php
@@ -13,6 +13,7 @@ class Request
     private $token = '';
     private $subdomain = '';
     private $client;
+    private $defaultOptions = ['allow_redirects' => false];
 
     public function __construct($subdomain, $token)
     {
@@ -35,57 +36,64 @@ class Request
 
     public function get($url = null, array $options = [])
     {
-        return $this->client->get($url . self::API_URL_EXT, [
-            'json' => $options,
-            'allow_redirects' => false
-        ]);
+        $params = $this->defaultOptions;
+        if (count($options) > 0) {
+            $params['json'] = $options;
+        }
+        return $this->client->get($url . self::API_URL_EXT, $params);
     }
 
     public function head($url = null, array $options = [])
     {
-        return $this->client->head($url . self::API_URL_EXT, [
-            'json' => $options,
-            'allow_redirects' => false
-        ]);
+        $params = $this->defaultOptions;
+        if (count($options) > 0) {
+            $params['json'] = $options;
+        }
+        return $this->client->head($url . self::API_URL_EXT, $params);
     }
 
     public function delete($url = null, array $options = [])
     {
-        return $this->client->delete($url . self::API_URL_EXT, [
-            'json' => $options,
-            'allow_redirects' => false
-        ]);
+        $params = $this->defaultOptions;
+        if (count($options) > 0) {
+            $params['json'] = $options;
+        }
+        return $this->client->delete($url . self::API_URL_EXT, $params);
     }
 
     public function put($url = null, array $options = [])
     {
-        return $this->client->put($url . self::API_URL_EXT, [
-            'json' => $options,
-            'allow_redirects' => false
-        ]);
+        $params = $this->defaultOptions;
+        if (count($options) > 0) {
+            $params['json'] = $options;
+        }
+        return $this->client->put($url . self::API_URL_EXT, $params);
     }
 
     public function patch($url = null, array $options = [])
     {
-        return $this->client->patch($url . self::API_URL_EXT, [
-            'json' => $options,
-            'allow_redirects' => false
-        ]);
+        $params = $this->defaultOptions;
+        if (count($options) > 0) {
+            $params['json'] = $options;
+        }
+        return $this->client->patch($url . self::API_URL_EXT, $params);
     }
 
     public function post($url = null, array $options = [])
     {
-        return $this->client->post($url . self::API_URL_EXT, [
-            'json' => $options,
-            'allow_redirects' => false
-        ]);
+        $params = $this->defaultOptions;
+        if (count($options) > 0) {
+            $params['json'] = $options;
+        }
+        return $this->client->post($url . self::API_URL_EXT, $params);
     }
 
     public function options($url = null, array $options = [])
     {
-        return $this->client->options($url . self::API_URL_EXT, [
-            'json' => $options,
-            'allow_redirects' => false
-        ]);
+        $params = $this->defaultOptions;
+        if (count($options) > 0) {
+            $params['json'] = $options;
+        }
+        return $this->client->options($url . self::API_URL_EXT, $params);
     }
 }
